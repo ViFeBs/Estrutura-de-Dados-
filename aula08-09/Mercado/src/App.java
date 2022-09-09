5b3f07ea268bd590d7211d1ec9a7290a7fefa225
BRUNO VIANA RUFINO <bruno.vrufino@senacsp.edu.br>
2022-09-08 22:13:29 -0300

    Lista de Compras

--- aula08-09/Mercado/src/App.java
+++ aula08-09/Mercado/src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Compra cm1 = new Compra(2,"Cafe",2,18.80);
+        Compra cm2 = new Compra(3,"Leite",4,45);
+        Compra cm3 = new Compra(1,"Açucar", 2, 12.50);
+
+        ListaCompra lstCompra = new ListaCompra();
+        lstCompra.inserirItem(cm1,0);
+        lstCompra.inserirItem(cm2,1);
+        lstCompra.inserirItem(cm3,2);
+
+        lstCompra.imprimir();
+
+        System.out.println(lstCompra.pesquisaItem(2));
+
+        lstCompra.removerItem(2);
+
+        lstCompra.imprimir();
+
+        lstCompra.computaCompra();
+
     }
 }
--- aula08-09/Mercado/src/ListaCompra.java
+++ aula08-09/Mercado/src/ListaCompra.java
@@ -17,4 +17,34 @@ public class ListaCompra {
             lstCompra.get(i).imprimir();
         }
     }
+
+    public void removerItem(int item){
+        lstCompra.remove(pesquisaItem(item));
+    }
+
+    public int pesquisaItem(int item){
+        int achado = -1;
+        for(int i = 0; i < lstCompra.size(); i++){
+            if(lstCompra.get(i).getItem() == item){
+                achado = i;
+                return achado;
+            }
+            
+        }
+        return achado;
+    }
+
+    public double computaCompra(){
+        double total = 0, valor =  0, qtde = 0;
+        for(int i = 0; i < lstCompra.size(); i++){
+            valor += lstCompra.get(i).getPreco();
+            qtde += lstCompra.get(i).getQtd();
+        }
+
+        total = valor + qtde;
+        System.out.println("Calculo de tudo: " + total + " Total Valor da Compra: " + valor + " Quantidade de Produtos comprados: " + qtde);
+
+        return total;
+    }
+
 }
