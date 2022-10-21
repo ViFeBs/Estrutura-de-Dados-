96790e68aa3f8560cd498d8258933dfc45f396bf
BRUNO VIANA RUFINO <bruno.vrufino@senacsp.edu.br>
2022-10-20 21:58:05 -0300

    Aula 20/10

---
+++ Exercicio20-10/MapaDicionario/src/Palavra.java
@@ -0,0 +1,30 @@
+public class Palavra {
+    private String palavra;
+    private String significado;
+    
+    public Palavra(String palavra, String significado) {
+        this.palavra = palavra;
+        this.significado = significado;
+    }
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    public void setPalavra(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public String getSignificado() {
+        return significado;
+    }
+
+    public void setSignificado(String significado) {
+        this.significado = significado;
+    }
+
+    @Override
+    public String toString() {
+        return "Palavra [palavra = " + palavra + ", significado = " + significado + "]";
+    }
+}
